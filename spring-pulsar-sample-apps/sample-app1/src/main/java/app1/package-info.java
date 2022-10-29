7d3b1a7f9ddaac5f5d7e4671594ccf3e440aabf3
Chris Bono <cbono@vmware.com>
2022-10-29 14:39:52 -0500

    Split sample apps into multi-modules
    Cherry pick https://github.com/spring-projects-experimental/spring-pulsar/commit/87e349066bf888fdd4fcdfaf8c998f49ccdd7449

---
+++ spring-pulsar-sample-apps/sample-app1/src/main/java/app1/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Package containing sample app for the framework.
+ */
+@NonNullApi
+@NonNullFields
+package app1;
+
+import org.springframework.lang.NonNullApi;
+import org.springframework.lang.NonNullFields;
