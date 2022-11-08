033d3aa8318e365896dbd5520f03a7d7941fbf3a
Soby Chacko <chackos@vmware.com>
2022-11-07 19:07:54 -0500

    Spring Pulsar AOT - initial changes
    Resolves https://github.com/spring-projects-experimental/spring-pulsar/issues/117

---
+++ spring-pulsar/src/main/java/org/springframework/pulsar/aot/PulsarRuntimeHints.java
@@ -0,0 +1,92 @@
+/*
+ * Copyright 2022 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.pulsar.aot;
+
+import java.util.HashSet;
+import java.util.TreeMap;
+import java.util.stream.Stream;
+
+import org.apache.pulsar.client.admin.internal.OffloadProcessStatusImpl;
+import org.apache.pulsar.client.admin.internal.PulsarAdminBuilderImpl;
+import org.apache.pulsar.client.api.Authentication;
+import org.apache.pulsar.client.api.AuthenticationDataProvider;
+import org.apache.pulsar.client.impl.conf.ClientConfigurationData;
+import org.apache.pulsar.client.impl.conf.ConsumerConfigurationData;
+import org.apache.pulsar.client.impl.conf.ProducerConfigurationData;
+import org.apache.pulsar.client.util.SecretsSerializer;
+import org.apache.pulsar.common.protocol.Commands;
+import org.apache.pulsar.shade.io.netty.buffer.AbstractByteBufAllocator;
+import org.apache.pulsar.shade.io.netty.channel.socket.nio.NioDatagramChannel;
+import org.apache.pulsar.shade.io.netty.channel.socket.nio.NioSocketChannel;
+import org.apache.pulsar.shade.io.netty.util.ReferenceCountUtil;
+
+import org.springframework.aot.hint.MemberCategory;
+import org.springframework.aot.hint.ReflectionHints;
+import org.springframework.aot.hint.RuntimeHints;
+import org.springframework.aot.hint.RuntimeHintsRegistrar;
+import org.springframework.aot.hint.TypeReference;
+import org.springframework.lang.Nullable;
+
+/**
+ * {@link RuntimeHintsRegistrar} for Spring for Apache Pulsar.
+ *
+ * @author Soby Chacko
+ */
+public class PulsarRuntimeHints implements RuntimeHintsRegistrar {
+
+	@Override
+	public void registerHints(RuntimeHints hints, @Nullable ClassLoader classLoader) {
+		ReflectionHints reflectionHints = hints.reflection();
+		Stream.of(HashSet.class, TreeMap.class, Authentication.class, AuthenticationDataProvider.class,
+				SecretsSerializer.class, NioSocketChannel.class, AbstractByteBufAllocator.class,
+				NioDatagramChannel.class, PulsarAdminBuilderImpl.class, OffloadProcessStatusImpl.class, Commands.class,
+				ReferenceCountUtil.class).forEach(
+						type -> reflectionHints.registerType(type,
+								builder -> builder.withMembers(MemberCategory.INVOKE_DECLARED_CONSTRUCTORS,
+										MemberCategory.INVOKE_DECLARED_METHODS,
+										MemberCategory.INTROSPECT_PUBLIC_METHODS)));
+
+		Stream.of(ClientConfigurationData.class, ConsumerConfigurationData.class, ProducerConfigurationData.class)
+				.forEach(type -> reflectionHints.registerType(type,
+						builder -> builder.withMembers(MemberCategory.INVOKE_DECLARED_CONSTRUCTORS,
+								MemberCategory.INVOKE_DECLARED_METHODS, MemberCategory.INTROSPECT_PUBLIC_METHODS,
+								MemberCategory.DECLARED_CLASSES, MemberCategory.DECLARED_FIELDS)));
+
+		Stream.of(
+				"org.apache.pulsar.shade.io.netty.util.internal.shaded.org.jctools.queues.BaseMpscLinkedArrayQueueProducerFields",
+				"org.apache.pulsar.shade.io.netty.util.internal.shaded.org.jctools.queues.BaseMpscLinkedArrayQueueConsumerFields",
+				"org.apache.pulsar.shade.io.netty.util.internal.shaded.org.jctools.queues.BaseMpscLinkedArrayQueueColdProducerFields",
+				"org.apache.pulsar.shade.io.netty.util.internal.shaded.org.jctools.queues.MpscArrayQueueProducerIndexField",
+				"org.apache.pulsar.shade.io.netty.util.internal.shaded.org.jctools.queues.MpscArrayQueueProducerLimitField",
+				"org.apache.pulsar.shade.io.netty.util.internal.shaded.org.jctools.queues.MpscArrayQueueConsumerIndexField")
+				.forEach(typeName -> reflectionHints.registerTypeIfPresent(classLoader, typeName,
+						MemberCategory.DECLARED_FIELDS));
+
+		Stream.of("com.github.benmanes.caffeine.cache.SSMSA", "com.github.benmanes.caffeine.cache.PSAMS")
+				.forEach(typeName -> reflectionHints.registerTypeIfPresent(classLoader, typeName,
+						MemberCategory.INVOKE_DECLARED_CONSTRUCTORS, MemberCategory.INVOKE_DECLARED_METHODS,
+						MemberCategory.INTROSPECT_PUBLIC_METHODS));
+
+		hints.proxies().registerJdkProxy(TypeReference.of("org.apache.pulsar.shade.io.netty.util.TimerTask"),
+				TypeReference.of("org.apache.pulsar.client.impl.ConnectionHandler$Connection"),
+				TypeReference.of("org.apache.pulsar.client.api.Producer"),
+				TypeReference.of("org.springframework.aop.SpringProxy"),
+				TypeReference.of("org.springframework.aop.framework.Advised"),
+				TypeReference.of("org.springframework.core.DecoratingProxy"));
+	}
+
+}
