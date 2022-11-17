745d1279b3bc0a85edee124937c678c8a5080a01
Christophe Bornet <cbornet@hotmail.com>
2022-11-17 18:34:26 +0100

    Add test for producer properties

--- spring-pulsar-spring-boot-autoconfigure/src/test/java/org/springframework/pulsar/autoconfigure/PulsarPropertiesTests.java
+++ spring-pulsar-spring-boot-autoconfigure/src/test/java/org/springframework/pulsar/autoconfigure/PulsarPropertiesTests.java
@@ -23,6 +23,11 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.apache.pulsar.client.api.CompressionType;
+import org.apache.pulsar.client.api.HashingScheme;
+import org.apache.pulsar.client.api.MessageRoutingMode;
+import org.apache.pulsar.client.api.ProducerAccessMode;
+import org.apache.pulsar.client.api.ProducerCryptoFailureAction;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
@@ -35,6 +40,7 @@ import org.springframework.boot.context.properties.source.MapConfigurationProper
  * Unit tests for {@link PulsarProperties}.
  *
  * @author Chris Bono
+ * @author Christophe Bornet
  */
 public class PulsarPropertiesTests {
 
@@ -95,6 +101,46 @@ public class PulsarPropertiesTests {
 			assertThatIllegalArgumentException().isThrownBy(() -> properties.buildAdminProperties())
 					.withMessageContaining(
 							"Cannot set both spring.pulsar.administration.authParams and spring.pulsar.administration.authentication.*");
+		}
+
+	}
+
+	@Nested
+	class ProducerPropertiesTests {
+
+		@Test
+		void producerProperties() {
+			Map<String, String> props = new HashMap<>();
+			props.put("spring.pulsar.producer.topic-name", "my-topic");
+			props.put("spring.pulsar.producer.producer-name", "my-producer");
+			props.put("spring.pulsar.producer.send-timeout", "2s");
+			props.put("spring.pulsar.producer.block-if-queue-full", "true");
+			props.put("spring.pulsar.producer.max-pending-messages", "3");
+			props.put("spring.pulsar.producer.max-pending-messages-across-partitions", "4");
+			props.put("spring.pulsar.producer.message-routing-mode", "CustomPartition");
+			props.put("spring.pulsar.producer.hashing-scheme", "Murmur3_32Hash");
+			props.put("spring.pulsar.producer.crypto-failure-action", "SEND");
+			props.put("spring.pulsar.producer.batching-max-publish-delay", "5s");
+			props.put("spring.pulsar.producer.batching-max-messages", "6");
+			props.put("spring.pulsar.producer.batching-enabled", "false");
+			props.put("spring.pulsar.producer.chunking-enabled", "true");
+			props.put("spring.pulsar.producer.compression-type", "LZ4");
+			props.put("spring.pulsar.producer.producer-access-mode", "Exclusive");
+
+			bind(props);
+			Map<String, Object> producerProps = properties.buildProducerProperties();
+
+			assertThat(producerProps).containsEntry("topicName", "my-topic")
+					.containsEntry("producerName", "my-producer").containsEntry("sendTimeoutMs", 2_000L)
+					.containsEntry("blockIfQueueFull", true).containsEntry("maxPendingMessages", 3)
+					.containsEntry("maxPendingMessagesAcrossPartitions", 4)
+					.containsEntry("messageRoutingMode", MessageRoutingMode.CustomPartition)
+					.containsEntry("hashingScheme", HashingScheme.Murmur3_32Hash)
+					.containsEntry("cryptoFailureAction", ProducerCryptoFailureAction.SEND)
+					.containsEntry("batchingMaxPublishDelayMicros", 5_000_000L).containsEntry("batchingMaxMessages", 6)
+					.containsEntry("batchingEnabled", false).containsEntry("chunkingEnabled", true)
+					.containsEntry("compressionType", CompressionType.LZ4)
+					.containsEntry("accessMode", ProducerAccessMode.Exclusive);
 		}
 
 	}
