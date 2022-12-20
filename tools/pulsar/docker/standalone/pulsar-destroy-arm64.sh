3fa7eac156ecc00d6bde157f820f768c285edb36
Chris Bono <cbono@vmware.com>
2022-12-20 17:01:07 -0600

    [Tools] Update Pulsar scripts for Arm64
    - Remove "arm64" suffix from volumes

--- tools/pulsar/docker/standalone/pulsar-destroy-arm64.sh
+++ tools/pulsar/docker/standalone/pulsar-destroy-arm64.sh
@@ -3,5 +3,5 @@
 echo "Finding and removing Pulsar container..."
 docker ps -a | grep "kezhenxu94/pulsar" | awk '{print $1}' | xargs docker rm -f
 echo "Removing Pulsar volumes..."
-docker volume rm pulsardata-arm64 pulsarconf-arm64
+docker volume rm pulsardata pulsarconf
 echo "All traces of Pulsar standalone removed"
