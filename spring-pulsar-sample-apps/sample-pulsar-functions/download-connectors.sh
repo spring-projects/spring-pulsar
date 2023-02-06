59a4d949646c3ec79630696c78e0d11db90cf0cd
Chris Bono <cbono@vmware.com>
2023-02-06 14:04:59 -0600

    Update Pulsar 2.10.3 (#315)

--- spring-pulsar-sample-apps/sample-pulsar-functions/download-connectors.sh
+++ spring-pulsar-sample-apps/sample-pulsar-functions/download-connectors.sh
@@ -2,6 +2,6 @@
 
 mkdir connectors
 cd connectors
-wget https://archive.apache.org/dist/pulsar/pulsar-2.10.2/connectors/pulsar-io-cassandra-2.10.2.nar
-wget https://archive.apache.org/dist/pulsar/pulsar-2.10.2/connectors/pulsar-io-rabbitmq-2.10.2.nar
+wget https://archive.apache.org/dist/pulsar/pulsar-2.10.3/connectors/pulsar-io-cassandra-2.10.3.nar
+wget https://archive.apache.org/dist/pulsar/pulsar-2.10.3/connectors/pulsar-io-rabbitmq-2.10.3.nar
 cd ..
