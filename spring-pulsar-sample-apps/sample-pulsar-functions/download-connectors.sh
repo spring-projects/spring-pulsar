64159ff350de734f3bccea48903557a0266685aa
Chris Bono <cbono@vmware.com>
2023-02-14 14:52:09 -0600

    Update Pulsar to 2.11.0 (#337)
    * User template vars for links to pulsar docs site
    * Update all code references to `2.11.0`

--- spring-pulsar-sample-apps/sample-pulsar-functions/download-connectors.sh
+++ spring-pulsar-sample-apps/sample-pulsar-functions/download-connectors.sh
@@ -2,6 +2,6 @@
 
 mkdir connectors
 cd connectors
-wget https://archive.apache.org/dist/pulsar/pulsar-2.10.3/connectors/pulsar-io-cassandra-2.10.3.nar
-wget https://archive.apache.org/dist/pulsar/pulsar-2.10.3/connectors/pulsar-io-rabbitmq-2.10.3.nar
+wget https://archive.apache.org/dist/pulsar/pulsar-2.11.0/connectors/pulsar-io-cassandra-2.11.0.nar
+wget https://archive.apache.org/dist/pulsar/pulsar-2.11.0/connectors/pulsar-io-rabbitmq-2.11.0.nar
 cd ..
