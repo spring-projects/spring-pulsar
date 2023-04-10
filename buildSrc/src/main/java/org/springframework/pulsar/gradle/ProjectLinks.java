86c95f883e4ed21bb85a2279df5794cf376af455
Chris Bono <cbono@vmware.com>
2023-04-10 16:05:33 -0500

    Replace refs from `spring-projects-experimental` to `spring-projects`

--- buildSrc/src/main/java/org/springframework/pulsar/gradle/ProjectLinks.java
+++ buildSrc/src/main/java/org/springframework/pulsar/gradle/ProjectLinks.java
@@ -2,12 +2,12 @@ package org.springframework.pulsar.gradle;
 
 public enum ProjectLinks {
 
-	HOMEPAGE("https://github.com/spring-projects-experimental/spring-pulsar"),
-	ISSUES("https://github.com/spring-projects-experimental/spring-pulsar/issues"),
-	CI("https://github.com/spring-projects-experimental/spring-pulsar/actions"),
-	SCM_URL("https://github.com/spring-projects-experimental/spring-pulsar"),
-	SCM_CONNECTION("https://github.com/spring-projects-experimental/spring-pulsar.git"),
-	SCM_DEV_CONNECTION("git@github.com:spring-projects-experimental/spring-pulsar.git");
+	HOMEPAGE("https://github.com/spring-projects/spring-pulsar"),
+	ISSUES("https://github.com/spring-projects/spring-pulsar/issues"),
+	CI("https://github.com/spring-projects/spring-pulsar/actions"),
+	SCM_URL("https://github.com/spring-projects/spring-pulsar"),
+	SCM_CONNECTION("https://github.com/spring-projects/spring-pulsar.git"),
+	SCM_DEV_CONNECTION("git@github.com:spring-projects/spring-pulsar.git");
 
 	private final String link;
 
