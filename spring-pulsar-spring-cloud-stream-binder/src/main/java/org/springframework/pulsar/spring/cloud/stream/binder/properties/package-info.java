a4caa2fa2b160d811a3f8f01ce29748313f924b3
Chris Bono <cbono@vmware.com>
2023-04-11 09:25:42 -0500

    Removes all non-core components from main branch (#390)
    * Prunes all non-core components
    
    Remove modules:
    - spring-pulsar-spring-boot-starter
    - spring-pulsar-reactive-spring-boot-starter
    - spring-pulsar-spring-boot-autoconfigure
    - spring-pulsar-spring-cloud-stream-binder
    
    - Remove config props plugins
    - Update docs to point to Spring Boot config props
    - Move intTest into spring-pulsar and spring-pulsar-reactive
    
    * Replace refs from `spring-projects-experimental` to `spring-projects`
    
    * Update spring-pulsar-spring-boot-starter to 0.2.1-SNAPSHOT

--- spring-pulsar-spring-cloud-stream-binder/src/main/java/org/springframework/pulsar/spring/cloud/stream/binder/properties/package-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * Package containing Spring Cloud Stream binder properties classes for Apache Pulsar.
- */
-@NonNullApi
-@NonNullFields
-package org.springframework.pulsar.spring.cloud.stream.binder.properties;
-
-import org.springframework.lang.NonNullApi;
-import org.springframework.lang.NonNullFields;
