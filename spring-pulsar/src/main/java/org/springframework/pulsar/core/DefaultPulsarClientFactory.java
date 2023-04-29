82f5bcbaf361b25fc559f120364199728528e32e
Chris Bono <cbono@vmware.com>
2023-04-29 11:04:22 -0500

    Port "Use builder to autoconfigure PulsarClient (#394)" from 0.2.x (#396)
    - Replace PulsarClientFactoryBean with PulsarClientFactory

---
+++ spring-pulsar/src/main/java/org/springframework/pulsar/core/DefaultPulsarClientFactory.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2022-2023 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.pulsar.core;
+
+import org.apache.pulsar.client.api.PulsarClient;
+import org.apache.pulsar.client.api.PulsarClientException;
+
+import org.springframework.util.Assert;
+
+/**
+ * Default implementation for {@link PulsarClientFactory}.
+ *
+ * @author Soby Chacko
+ * @author Chris Bono
+ */
+public class DefaultPulsarClientFactory implements PulsarClientFactory {
+
+	private final PulsarClientBuilderCustomizer customizer;
+
+	/**
+	 * Construct a factory that creates clients using a default Pulsar client builder with
+	 * no modifications other than the specified service url.
+	 * @param serviceUrl the service url
+	 */
+	public DefaultPulsarClientFactory(String serviceUrl) {
+		this((clientBuilder -> clientBuilder.serviceUrl(serviceUrl)));
+	}
+
+	/**
+	 * Construct a factory that creates clients using a customized Pulsar client builder.
+	 * @param customizer the customizer to apply to the builder
+	 */
+	public DefaultPulsarClientFactory(PulsarClientBuilderCustomizer customizer) {
+		Assert.notNull(customizer, "customizer must not be null");
+		this.customizer = customizer;
+	}
+
+	@Override
+	public PulsarClient createClient() throws PulsarClientException {
+		var clientBuilder = PulsarClient.builder();
+		this.customizer.customize(clientBuilder);
+		return clientBuilder.build();
+	}
+
+}
---
+++ spring-pulsar/src/main/java/org/springframework/pulsar/core/PulsarClientFactory.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2023-2023 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.pulsar.core;
+
+import org.apache.pulsar.client.api.PulsarClient;
+import org.apache.pulsar.client.api.PulsarClientException;
+
+/**
+ * Pulsar client factory interface.
+ *
+ * @author Soby Chacko
+ * @author Chris Bono
+ */
+public interface PulsarClientFactory {
+
+	/**
+	 * Create a client.
+	 * @return the created client instance
+	 * @throws PulsarClientException if an error occurs creating the client
+	 */
+	PulsarClient createClient() throws PulsarClientException;
+
+}
