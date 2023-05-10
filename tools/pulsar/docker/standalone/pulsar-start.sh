1268b8bd7683e2cf88fe04a71d8c407ee9c78fb9
Chris Bono <cbono@vmware.com>
2023-05-10 18:03:19 -0500

    Update Pulsar 3.0.0

--- tools/pulsar/docker/standalone/pulsar-start.sh
+++ tools/pulsar/docker/standalone/pulsar-start.sh
@@ -3,5 +3,5 @@
 docker run -it -p 6650:6650 -p 8080:8080 \
   --mount source=pulsardata,target=/pulsar/data \
   --mount source=pulsarconf,target=/pulsar/conf \
-  apachepulsar/pulsar:2.11.0 \
+  apachepulsar/pulsar:3.0.0 \
   bin/pulsar standalone
