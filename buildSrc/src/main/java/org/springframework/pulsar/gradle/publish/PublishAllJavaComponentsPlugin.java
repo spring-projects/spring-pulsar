893161b6014234a0ca0b00ddf72c21ffd7d90a07
Chris Bono <cbono@vmware.com>
2023-09-30 06:31:30 -0500

    Do not sign directories in artifacts (Gradle 8 workaround) (#465)

--- buildSrc/src/main/java/org/springframework/pulsar/gradle/publish/PublishAllJavaComponentsPlugin.java
+++ buildSrc/src/main/java/org/springframework/pulsar/gradle/publish/PublishAllJavaComponentsPlugin.java
@@ -1,10 +1,13 @@
 package org.springframework.pulsar.gradle.publish;
 
+import java.util.stream.Collectors;
+
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
 import org.gradle.api.plugins.JavaPlatformPlugin;
 import org.gradle.api.plugins.JavaPlugin;
 import org.gradle.api.publish.PublishingExtension;
+import org.gradle.api.publish.maven.MavenArtifactSet;
 import org.gradle.api.publish.maven.MavenPublication;
 import org.gradle.api.publish.maven.plugins.MavenPublishPlugin;
 
@@ -17,6 +20,8 @@ public class PublishAllJavaComponentsPlugin implements Plugin<Project> {
 			publishing.getPublications().create("mavenJava", MavenPublication.class, maven -> {
 				project.getPlugins().withType(JavaPlugin.class, (plugin) -> {
 					maven.from(project.getComponents().getByName("java"));
+					// Workaround issue where build/classes and build/resources are attempting to be signed
+					maven.setArtifacts(maven.getArtifacts().stream().filter((ma) -> ma.getFile().isFile()).collect(Collectors.toSet()));
 				});
 				project.getPlugins().withType(JavaPlatformPlugin.class, (plugin) -> {
 					maven.from(project.getComponents().getByName("javaPlatform"));
