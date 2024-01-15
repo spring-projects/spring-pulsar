00175cad7fe8ec4761c58ba1f3e38fabb224cd53
Chris Bono <cbono@vmware.com>
2024-01-14 21:48:09 -0600

    [Samples] Make sample-reactive testable

--- spring-pulsar-sample-apps/sample-reactive/src/main/java/com/example/package-info.java
+++ spring-pulsar-sample-apps/sample-reactive/src/main/java/com/example/package-info.java
@@ -3,7 +3,7 @@
  */
 @NonNullApi
 @NonNullFields
-package org.springframework.pulsar.example;
+package com.example;
 
 import org.springframework.lang.NonNullApi;
 import org.springframework.lang.NonNullFields;
---
+++ spring-pulsar-sample-apps/sample-reactive/src/test/java/com/example/ReactiveSpringPulsarBootAppTests.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright 2012-2024 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.IntStream;
+
+import com.example.ReactiveSpringPulsarBootApp.Foo;
+import org.awaitility.Awaitility;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.system.CapturedOutput;
+import org.springframework.boot.test.system.OutputCaptureExtension;
+import org.springframework.pulsar.test.support.PulsarTestContainerSupport;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@ExtendWith(OutputCaptureExtension.class)
+class ReactiveSpringPulsarBootAppTests implements PulsarTestContainerSupport {
+
+	@DynamicPropertySource
+	static void pulsarProperties(DynamicPropertyRegistry registry) {
+		registry.add("spring.pulsar.client.service-url", PULSAR_CONTAINER::getPulsarBrokerUrl);
+		registry.add("spring.pulsar.admin.service-url", PULSAR_CONTAINER::getHttpServiceUrl);
+	}
+
+	@Test
+	void reactiveTemplateWithSimpleReactiveListener(CapturedOutput output) {
+		verifyProduceConsume(output,10, (i) -> "ReactiveTemplateWithSimpleReactiveListener:" + i);
+	}
+
+	@Test
+	void reactiveTemplateWithStreamingReactiveListener(CapturedOutput output) {
+		verifyProduceConsume(output,10, (i) -> new Foo("Foo-" + i, "Bar-" + i));
+	}
+
+	@Test
+	void reactiveTemplateWithImperativeListener(CapturedOutput output) {
+		verifyProduceConsume(output,10, (i) -> "ReactiveTemplateWithImperativeListener:" + i);
+	}
+
+	private void verifyProduceConsume(CapturedOutput output, int numExpectedMessages,
+			Function<Integer, Object> expectedMessageFactory) {
+		List < String > expectedOutput = new ArrayList<>();
+		IntStream.range(0, numExpectedMessages).forEachOrdered((i) -> {
+			var msg = expectedMessageFactory.apply(i);
+			expectedOutput.add("++++++PRODUCE %s------".formatted(msg));
+			expectedOutput.add("++++++CONSUME %s------".formatted(msg));
+		});
+		Awaitility.waitAtMost(Duration.ofSeconds(15))
+				.untilAsserted(() -> assertThat(output).contains(expectedOutput));
+	}
+}
