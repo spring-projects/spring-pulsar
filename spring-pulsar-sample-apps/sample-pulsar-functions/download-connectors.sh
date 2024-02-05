0da4142b48d99c91913a4ad988aa6848be7daaba
Chris Bono <cbono@vmware.com>
2024-02-05 10:09:02 -0600

    Update to Pulsar 3.2.0 (#564)
    Resolves #563

--- spring-pulsar-sample-apps/sample-pulsar-functions/download-connectors.sh
+++ spring-pulsar-sample-apps/sample-pulsar-functions/download-connectors.sh
@@ -2,6 +2,6 @@
 
 mkdir connectors
 cd connectors
-wget https://archive.apache.org/dist/pulsar/pulsar-3.1.2/connectors/pulsar-io-cassandra-3.1.2.nar
-wget https://archive.apache.org/dist/pulsar/pulsar-3.1.2/connectors/pulsar-io-rabbitmq-3.1.2.nar
+wget https://archive.apache.org/dist/pulsar/pulsar-3.2.0/connectors/pulsar-io-cassandra-3.2.0.nar
+wget https://archive.apache.org/dist/pulsar/pulsar-3.2.0/connectors/pulsar-io-rabbitmq-3.2.0.nar
 cd ..
--- spring-pulsar-test/src/main/java/org/springframework/pulsar/test/support/PulsarTestContainerSupport.java
+++ spring-pulsar-test/src/main/java/org/springframework/pulsar/test/support/PulsarTestContainerSupport.java
@@ -41,7 +41,7 @@ public interface PulsarTestContainerSupport {
 	}
 
 	static DockerImageName getPulsarImage() {
-		return DockerImageName.parse("apachepulsar/pulsar:3.1.2");
+		return DockerImageName.parse("apachepulsar/pulsar:3.2.0");
 	}
 
 	static String getHttpServiceUrl() {
