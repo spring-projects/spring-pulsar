85a1f2c821d993f97fb5a7e97897f3df01e53234
Chris Bono <chris.bono@gmail.com>
2024-03-07 15:16:25 -0600

    Publish the spring-pulsar-test module
    This commit makes the spring-pulsar-test module available externally.
    
    Additionally:
    * Add missing package-info.java in several packages
    * Update doc on how to use PulsarTestContainerSupport

---
+++ spring-pulsar-reactive/src/main/java/org/springframework/pulsar/reactive/support/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Package containing support classes for processing Pulsar messages.
+ */
+@NonNullApi
+@NonNullFields
+package org.springframework.pulsar.reactive.support;
+
+import org.springframework.lang.NonNullApi;
+import org.springframework.lang.NonNullFields;
---
+++ spring-pulsar-test/src/main/java/org/springframework/pulsar/test/support/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * Package containing convenience utilities for testing Spring for Apache Pulsar
+ * applications.
+ * @since 1.1.0
+ */
+@NonNullApi
+@NonNullFields
+package org.springframework.pulsar.test.support;
+
+import org.springframework.lang.NonNullApi;
+import org.springframework.lang.NonNullFields;
