6103f4bd94d1386ab56fde434afdb387b23f9548
Chris Bono <chris.bono@gmail.com>
2024-04-14 11:42:17 -0500

    Update to Pulsar 3.2.2
    Resolves #642

--- spring-pulsar-sample-apps/sample-pulsar-functions/download-connectors.sh
+++ spring-pulsar-sample-apps/sample-pulsar-functions/download-connectors.sh
@@ -2,6 +2,6 @@
 
 mkdir connectors
 cd connectors
-wget https://archive.apache.org/dist/pulsar/pulsar-3.2.1/connectors/pulsar-io-cassandra-3.2.1.nar
-wget https://archive.apache.org/dist/pulsar/pulsar-3.2.1/connectors/pulsar-io-rabbitmq-3.2.1.nar
+wget https://archive.apache.org/dist/pulsar/pulsar-3.2.2/connectors/pulsar-io-cassandra-3.2.2.nar
+wget https://archive.apache.org/dist/pulsar/pulsar-3.2.2/connectors/pulsar-io-rabbitmq-3.2.2.nar
 cd ..
