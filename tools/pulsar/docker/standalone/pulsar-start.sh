a2e58fe53a346a33c2d3adfd3754039086592f3a
Chris Bono <chris.bono@gmail.com>
2024-05-17 19:52:55 -0500

    Update Pulsar to 3.2.3

--- tools/pulsar/docker/standalone/pulsar-start.sh
+++ tools/pulsar/docker/standalone/pulsar-start.sh
@@ -3,5 +3,5 @@
 docker run -it -p 6650:6650 -p 8080:8080 \
   --mount source=pulsardata,target=/pulsar/data \
   --mount source=pulsarconf,target=/pulsar/conf \
-  apachepulsar/pulsar:3.2.2 \
+  apachepulsar/pulsar:3.2.3 \
   bin/pulsar standalone
