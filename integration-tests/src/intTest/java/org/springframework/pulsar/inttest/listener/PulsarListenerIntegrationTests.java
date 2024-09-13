0f6136fec71a9bb1ff9a5c69a977c14e719e6f20
Chris Bono <chris.bono@gmail.com>
2024-09-12 22:32:54 -0500

    Ensure default topics are fully qualified
    Prior to this commit, only the topics specified on the `@PulsarListener`
    were being fully qualified in the `DefaultPulsarConsumerFactory`. With
    this change all topics (including the default topics driven by the
    Spring Boot `spring.pulsar.consumer.topics` config prop) are
    fully-qualified.
    .

--- integration-tests/src/intTest/java/org/springframework/pulsar/inttest/listener/PulsarListenerIntegrationTests.java
+++ integration-tests/src/intTest/java/org/springframework/pulsar/inttest/listener/PulsarListenerIntegrationTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2022-2023 the original author or authors.
+ * Copyright 2022-2024 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -160,7 +160,7 @@ class PulsarListenerIntegrationTests implements PulsarTestContainerSupport {
 			public void listen(String ignored, Consumer<String> consumer) {
 				assertThat(consumer).extracting("conf", InstanceOfAssertFactories.type(ConsumerConfigurationData.class))
 					.satisfies((conf) -> {
-						assertThat(conf.getSingleTopic()).isEqualTo("plit-config-props-topic-dev");
+						assertThat(conf.getSingleTopic()).isEqualTo("persistent://public/default/plit-config-props-topic-dev");
 						assertThat(conf.getSubscriptionType()).isEqualTo(SubscriptionType.Shared);
 						assertThat(conf.getSubscriptionName()).isEqualTo("plit-config-props-subs-dev");
 					});
