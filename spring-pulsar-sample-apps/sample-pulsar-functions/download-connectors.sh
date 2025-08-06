f742642bcf40f31d979edbf3bdbe5f365341af9a
onobc <chris.bono@gmail.com>
2025-08-06 17:59:35 -0500

    Update to Pulsar 3.3.8
    Signed-off-by: onobc <chris.bono@gmail.com>

--- spring-pulsar-sample-apps/sample-pulsar-functions/download-connectors.sh
+++ spring-pulsar-sample-apps/sample-pulsar-functions/download-connectors.sh
@@ -2,6 +2,6 @@
 
 mkdir connectors
 cd connectors
-wget https://archive.apache.org/dist/pulsar/pulsar-3.3.6/connectors/pulsar-io-cassandra-3.3.6.nar
-wget https://archive.apache.org/dist/pulsar/pulsar-3.3.6/connectors/pulsar-io-rabbitmq-3.3.6.nar
+wget https://archive.apache.org/dist/pulsar/pulsar-3.3.8/connectors/pulsar-io-cassandra-3.3.8.nar
+wget https://archive.apache.org/dist/pulsar/pulsar-3.3.8/connectors/pulsar-io-rabbitmq-3.3.8.nar
 cd ..
