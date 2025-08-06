f9e05e7e72e457bc98448d6c48ae271417cf6962
onobc <chris.bono@gmail.com>
2025-08-06 17:59:35 -0500

    Update to Pulsar 3.3.8
    Signed-off-by: onobc <chris.bono@gmail.com>

--- tools/pulsar/docker/standalone/pulsar-start.sh
+++ tools/pulsar/docker/standalone/pulsar-start.sh
@@ -3,5 +3,5 @@
 docker run -it -p 6650:6650 -p 8080:8080 \
   --mount source=pulsardata,target=/pulsar/data \
   --mount source=pulsarconf,target=/pulsar/conf \
-  apachepulsar/pulsar:3.3.6 \
+  apachepulsar/pulsar:3.3.8 \
   bin/pulsar standalone
