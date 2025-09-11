c4332fc52df23381959f0c25d8c007c6f51f40c5
onobc <chris.bono@gmail.com>
2025-09-11 16:06:57 -0500

    Update to Pulsar 4.1.0
    Signed-off-by: onobc <chris.bono@gmail.com>

--- spring-pulsar-sample-apps/sample-pulsar-functions/download-connectors.sh
+++ spring-pulsar-sample-apps/sample-pulsar-functions/download-connectors.sh
@@ -2,6 +2,6 @@
 
 mkdir connectors
 cd connectors
-wget https://archive.apache.org/dist/pulsar/pulsar-4.0.5/connectors/pulsar-io-cassandra-4.0.5.nar
-wget https://archive.apache.org/dist/pulsar/pulsar-4.0.5/connectors/pulsar-io-rabbitmq-4.0.5.nar
+wget https://archive.apache.org/dist/pulsar/pulsar-4.1.0/connectors/pulsar-io-cassandra-4.1.0.nar
+wget https://archive.apache.org/dist/pulsar/pulsar-4.1.0/connectors/pulsar-io-rabbitmq-4.1.0.nar
 cd ..
--- tools/pulsar/docker/standalone/pulsar-start.sh
+++ tools/pulsar/docker/standalone/pulsar-start.sh
@@ -3,5 +3,5 @@
 docker run -it -p 6650:6650 -p 8080:8080 \
   --mount source=pulsardata,target=/pulsar/data \
   --mount source=pulsarconf,target=/pulsar/conf \
-  apachepulsar/pulsar:4.0.6 \
+  apachepulsar/pulsar:4.1.0 \
   bin/pulsar standalone
