f35a8611ebb4e4d434f0d14729654fd0b0d3b464
onobc <chris.bono@gmail.com>
2026-03-15 15:55:53 -0500

    Update Pulsar to `4.1.3`
    Signed-off-by: onobc <chris.bono@gmail.com>

--- spring-pulsar-sample-apps/sample-pulsar-functions/download-connectors.sh
+++ spring-pulsar-sample-apps/sample-pulsar-functions/download-connectors.sh
@@ -2,6 +2,6 @@
 
 mkdir connectors
 cd connectors
-wget https://archive.apache.org/dist/pulsar/pulsar-4.1.2/connectors/pulsar-io-cassandra-4.1.2.nar
-wget https://archive.apache.org/dist/pulsar/pulsar-4.1.2/connectors/pulsar-io-rabbitmq-4.1.2.nar
+wget https://archive.apache.org/dist/pulsar/pulsar-4.1.3/connectors/pulsar-io-cassandra-4.1.3.nar
+wget https://archive.apache.org/dist/pulsar/pulsar-4.1.3/connectors/pulsar-io-rabbitmq-4.1.3.nar
 cd ..
--- spring-pulsar-test/src/main/java/org/springframework/pulsar/test/support/PulsarTestContainerSupport.java
+++ spring-pulsar-test/src/main/java/org/springframework/pulsar/test/support/PulsarTestContainerSupport.java
@@ -33,7 +33,7 @@ public interface PulsarTestContainerSupport {
 	PulsarContainer PULSAR_CONTAINER = new PulsarContainer(getPulsarImage());
 
 	static DockerImageName getPulsarImage() {
-		return DockerImageName.parse("apachepulsar/pulsar:4.1.2");
+		return DockerImageName.parse("apachepulsar/pulsar:4.1.3");
 	}
 
 	@BeforeAll
